d920a59a664c7348868055ac5ec74e6e5ced1f1c
Radifan <frutami19@gmail.com>
2024-12-12 15:11:33 +0700

    added topapbar

--- app/src/main/java/com/example/jejak_sejarah/ui/MainScreen.kt
+++ app/src/main/java/com/example/jejak_sejarah/ui/MainScreen.kt
@@ -1,11 +1,33 @@
 package com.example.jejak_sejarah.ui
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.rememberNavController
 import com.example.jejak_sejarah.ui.navigation.AppNavigation
+import com.example.jejak_sejarah.ui.navigation.BottomNavigationBar
+import com.example.jejak_sejarah.ui.theme.darkBrown
 
 @Composable
 fun MainScreen() {
     val navController = rememberNavController()
-    AppNavigation(navController = navController)
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(text = "JEJAK SEJARAH", fontWeight= FontWeight.Bold, color = Color.White)
+                },
+                backgroundColor = darkBrown
+            )
+        },
+        bottomBar = {
+            BottomNavigationBar(navController)
+        }
+    ) { innerPadding ->
+        AppNavigation(navController = navController, modifier = Modifier.padding(innerPadding).padding(top = 24.dp))
+    }
 }
--- app/src/main/java/com/example/jejak_sejarah/ui/navigation/AppNavigation.kt
+++ app/src/main/java/com/example/jejak_sejarah/ui/navigation/AppNavigation.kt
@@ -5,24 +5,21 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import com.example.jejak_sejarah.ui.home.HomeScreen
 import com.example.jejak_sejarah.ui.leaderboard.LeaderboardScreen
-import com.example.jejak_sejarah.ui.navigation.BottomNavigationBar
 import com.example.jejak_sejarah.ui.profile.ProfileScreen
 
 @Composable
-fun AppNavigation(navController: NavHostController) {
-    // Scaffold dengan bottom bar (Navbar)
+fun AppNavigation(navController: NavHostController, modifier:Modifier) {
     Scaffold(
         modifier = Modifier.fillMaxSize(),
-        bottomBar = { BottomNavigationBar(navController = navController) }, // Navbar di bawah
+        bottomBar = { BottomNavigationBar(navController = navController) },
         content = { paddingValues ->
-            // Menambahkan padding agar konten tidak tertutup navbar
             NavHost(navController = navController, startDestination = "home") {
-                // Menambahkan rute untuk setiap halaman
                 composable("home") { HomeScreen(modifier = Modifier.padding(paddingValues)) }
                 composable("leaderboard") { LeaderboardScreen(modifier = Modifier.padding(paddingValues)) }
                 composable("profile") { ProfileScreen(modifier = Modifier.padding(paddingValues)) }
